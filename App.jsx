45738da5e4c953540166c1f290a7ec11c10c855b
malinimalini2808 <malinimalini2801@gmail.com>
2026-04-23 20:09:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import User from "./User";
+
+let App = () => {
+    return (
+        <div>
+            {/* <h1>App Component</h1>
+            <Demo/> */}
+            <User />
+        </div>
+    );
+};
+ 
+export default App;
